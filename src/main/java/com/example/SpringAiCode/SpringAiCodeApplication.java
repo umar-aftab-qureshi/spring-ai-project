fcb550f962f3208de80e06b3140afc98b7095195
Umar Aftab <umerqureshi800@gmail.com>
2026-02-14 13:47:52 +0500

    Spring AI Starter Project

---
+++ src/main/java/com/example/SpringAiCode/SpringAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAiCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringAiCode/SpringAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAiCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
